864197b4fda2a7b843dee1ac215ea45a30c122a0
audisyzhn <audi17042002@gmail.com>
2019-09-26 11:56:01 +0700

    Initial commit

---
+++ app/src/main/java/com/smkn4/pwpb/view/activity_splash.java
@@ -0,0 +1,34 @@
+package com.smkn4.pwpb.view;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.smkn4.pwpb.R;
+
+public class activity_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+        Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        ImageView iv =(ImageView)findViewById(R.id.logo);
+        //tv hasn't been finished yet
+        iv.startAnimation(animFadeIn);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
